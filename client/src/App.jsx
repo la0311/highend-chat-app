0a08480320a020da587452f1e97d383e57fbda0d
La <35848139+la0311@users.noreply.github.com>
2023-04-12 21:42:13 +0700

    Basic setup

---
+++ client/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import {StreamChat} from 'stream-chat'
+import {Chat} from 'stream-chat-react'
+import Cookies from 'universal-cookie'
+
+import { ChannelContainer, ChannelListContainer } from './components'
+
+const apiKey = import.meta.env.STREAMCHAT_API_KEY
+const client= StreamChat.getInstance(apiKey)
+
+const App = () => {
+  return (
+    <div className='app__wrapper'>
+    <Chat client={client} theme='team light'>
+      <ChannelListContainer/>
+      <ChannelContainer/>
+    </Chat>
+    </div>
+  )
+}
+
+export default App
